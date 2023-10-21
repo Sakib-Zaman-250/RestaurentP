3c6ff1ca142f6e37b84628ee1950b669599a5e14
Sakib-Zaman-250 <sakibzaman250@gmail.com>
2023-10-21 23:15:43 +0600

    category add

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,8 +1,29 @@
+import { pizzas } from '@/data'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const CategoryPage = () => {
   return (
-    <div>Category Page</div>
+    <div className='flex flex-wrap text-red-500 '> 
+    {pizzas.map(item=>(
+
+      <Link className=' w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4' href={`/product/${item.id}`} key={item.id}>
+          {item.img && 
+          <div className='relative h-[80%] '>
+            <Image src={item.img} alt='' fill className='object-contain'/>
+            </div>}
+            <div className='flex items-center justify-between font-bold'>
+              <h1>{item.title}</h1>
+              <h2>${item.price}</h2>
+              <button className='uppercase bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
+            </div>
+
+
+      </Link>
+
+    ))}
+    </div>
   )
 }
 
